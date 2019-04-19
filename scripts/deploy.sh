4d9e9bea76f78889a29c849618d7d8f7521b0f2b
RubbaBoy <adamyarris@yahoo.com>
2019-04-18 22:17:56 -0400

    Wow, almost worked first try Just had to stop it from uploading the "www" folder itself too

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASS" $FTP_HOST /var/www/newocr.dev/public_html www/
+cd www
+ncftpput -R -v -u "$FTP_USER" -p "$FTP_PASS" $FTP_HOST /var/www/newocr.dev/public_html ./
